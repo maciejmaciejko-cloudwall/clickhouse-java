6d285310220155322e21ca61916e2082ba3667aa
Maciej Maciejko <maciej.maciejko@getindata.com>
2024-01-16 08:55:47 +0100

    initialization fix

--- clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthorizer.java
+++ clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthorizer.java
@@ -20,6 +20,7 @@ public class GssAuthorizer {
         GSSName gssName = manager.createName(serverName + "@" + host, GSSName.NT_HOSTBASED_SERVICE);
         Oid krb5SpnegoOid = new Oid("1.3.6.1.5.5.2");
         this.clientContext = manager.createContext(gssName, krb5SpnegoOid, null, GSSContext.DEFAULT_LIFETIME);
+        this.inToken = new byte[0];
     }
 
     public boolean isEstablished() {
