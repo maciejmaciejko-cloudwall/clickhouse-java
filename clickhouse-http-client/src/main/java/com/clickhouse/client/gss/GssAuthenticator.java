6e799fb67a48acb2d9b74a2c0bc2159365f2bf82
Maciej Maciejko <maciej.maciejko@getindata.com>
2024-01-17 16:45:25 +0100

    refactor

--- clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthenticator.java
+++ clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthenticator.java
@@ -10,23 +10,25 @@ import org.ietf.jgss.Oid;
 
 public class GssAuthenticator {
 
-    private final GSSContext secContext;
+    // private final GSSContext secContext;
+    private final String user;
+    private final String serverName;
+    private final String host;
 
     public GssAuthenticator(String user, String serverName, String host) throws GSSException {
+        this.user = user;
+        this.serverName = serverName;
+        this.host = host;
+
+    }
+
+    public String getAuthToken() throws GSSException {
         GSSManager manager = GSSManager.getInstance();
         GSSName gssServerName = manager.createName(serverName + "@" + host, GSSName.NT_HOSTBASED_SERVICE);
         Oid krb5SpnegoOid = new Oid("1.3.6.1.5.5.2");
         GSSName gssClientName = manager.createName(user, GSSName.NT_USER_NAME);
         GSSCredential clientCreds = manager.createCredential(gssClientName, 8 * 3600, krb5SpnegoOid, GSSCredential.INITIATE_ONLY);
-
-        this.secContext = manager.createContext(gssServerName, krb5SpnegoOid, clientCreds, GSSContext.DEFAULT_LIFETIME);
-    }
-
-    public String getAuthToken() throws GSSException {
+        GSSContext secContext = manager.createContext(gssServerName, krb5SpnegoOid, clientCreds, GSSContext.DEFAULT_LIFETIME);
         return Base64.encodeBase64String(secContext.initSecContext(new byte[0], 0, 0));
-    }
-
-    public boolean isEstablished() {
-        return secContext.isEstablished();
     }
 }
